c02519280b9ac8cd16ebefc690623a890d8390f0
Matt Venn <matt@mattvenn.net>
2020-11-02 13:02:35 +0100

    fix comments

--- firmware.s
+++ firmware.s
@@ -1,2 +1,3 @@
-addi    x1, x1, 0x10
-sw      x1, 0x00 (zero)
+addi    x1, zero, 0         # initialise x1 reg to 0
+addi    x1, x1, 0x05        # add 0x05 to x1
+sw      x1, 0x00 (zero)     # write x1 register to memory at address 0
